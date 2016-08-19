0b1641e85661182c3901f5f8655858ba8514ddde
Vikas Kumar <vkmangal1996@gmail.com>
2016-08-19 05:16:32 -0700

    Create Sol_4.cpp

---
+++ Sol_4.cpp
@@ -0,0 +1,156 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t,n,i,j,k,b,u,kp;
+	scanf("%d",&t);
+	while(t--){
+		scanf("%d",&n);
+		vector<vector<int> >x;
+		vector<vector<int> >y;
+		vector<vector<int> >z;
+		vector<vector<int> >temp;
+		x.clear();y.clear();temp.clear();
+		for(i=0;i<n;i++)
+		{
+			vector<int>gh;gh.clear();
+			for(j=0;j<n;j++)
+			{
+				scanf("%d",&u);
+				gh.push_back(u);
+			}
+			x.push_back(gh);
+		}
+		for(i=0;i<n;i++)
+		{
+			vector<int>gh;gh.clear();
+			for(j=0;j<n;j++)
+			{
+				scanf("%d",&u);
+				gh.push_back(u);
+			}
+			y.push_back(gh);
+		}
+		for(i=0;i<n;i++)
+		{
+			vector<int>gh;gh.clear();
+			for(j=0;j<n;j++)
+			{
+				scanf("%d",&u);
+				gh.push_back(u);
+			}
+			z.push_back(gh);
+		}
+		scanf("%d",&kp);
+		int plag=0,ind1,ind2;
+		for(i=0;i<n;i++)
+		{
+			for(j=0;j<n;j++)
+			{
+				if(kp==x[i][j])
+				{
+					ind1=i;ind2=j;plag=1;break;
+				}
+			}
+			if(plag==1) break;
+		}
+		queue<vector<vector<int> > >a;
+		while(!a.empty())a.pop();
+		stack<vector<vector<int> > >s;
+		while(!s.empty())s.pop();
+		map<vector<vector<int> >,vector<vector<int> > >par;
+		map<vector<vector<int> >,bool>visit;
+		a.push(x);
+		vector<vector<int> >tull(n,vector<int>(n));tull.clear();
+		par[x]=tull;
+		visit[x]=true;
+		bool gflag=false;
+		while(!a.empty())
+		{
+			temp=a.front();
+			a.pop();
+			if(temp==z || (temp==y && temp[ind1][ind2]==kp))
+			{
+				while(temp != tull)
+				{
+					s.push(temp);
+					temp=par[temp];
+				}
+				while(!s.empty())
+				{
+					vector<vector<int> >temp1(n,vector<int>(n));
+					temp1=s.top();
+					s.pop();
+					for(i=0;i<n;i++)
+						for(j=0;j<n;j++)
+							printf("%d ",temp1[i][j]);
+					printf("\n");
+				}
+				break;
+			}
+			/*if(!gflag && temp==y && temp[ind1][ind2]==kp)
+			{
+				while(temp != tull)
+				{
+					s.push(temp);
+					temp=par[temp];
+				}
+				while(!s.empty())
+				{
+					vector<vector<int> >temp1(n,vector<int>(n));
+					temp1=s.top();
+					s.pop();
+					for(i=0;i<n;i++)
+						for(j=0;j<n;j++)
+							printf("%d ",temp1[i][j]);
+					printf("\n");
+				}
+				break;
+			}
+			else if(!gflag && temp==y)
+			{
+				while(!a.empty())a.pop();
+				a.push(x);
+				temp=a.front();
+				a.pop();
+				gflag=true;
+			}*/
+			for(i=0;i<n;i++)
+			{
+				vector<vector<int> >temp2(n,vector<int>(n));
+				for(b=1;b<=n;b++)
+				{
+					temp2=temp;
+					rotate(temp2[i].begin(),temp2[i].end()-b,temp2[i].end());
+					if(visit.find(temp2)==visit.end())
+					{
+						par[temp2]=temp;
+						a.push(temp2);
+						visit[temp2]=true;
+					}
+				}
+			}
+			for(i=0;i<n;i++)
+			{
+				vector<vector<int> >temp2(n,vector<int>(n));
+				vector<int>arr(n);
+				for(b=1;b<=n;b++)
+				{
+					temp2=temp;
+					for(j=0;j<n;j++)
+						arr[j]=temp2[j][i];
+					rotate(arr.begin(),arr.end()-b,arr.end());
+					for(j=0;j<n;j++)
+						temp2[j][i]=arr[j];
+					if(visit.find(temp2)==visit.end())
+					{
+						par[temp2]=temp;
+						a.push(temp2);
+						visit[temp2]=true;
+					}
+				}
+			}
+		}
+	}
+	return 0;
+}
